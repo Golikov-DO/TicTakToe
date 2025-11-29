1234578f6c82266bad5041e3af79bf4b99682c20
Xaxatumbik <xaxatumbik@gmail.com>
2025-11-29 13:50:02 +0300

    Add constants.

---
+++ src/constants/GameConstants.java
@@ -0,0 +1,11 @@
+package constants;
+
+public class GameConstants {
+    public static final int FIELD_EMPTY = 0;
+    public static final int FIELD_X = 10;
+    public static final int FIELD_O = 200;
+    public static final String WINER_O = "Нолики выиграли!";
+    public static final String WINER_X = "Крестики выиграли!";
+    public static final String NON_WINER = "Ничья!";
+    public static final String QUESTION = "Начать заново?";
+}
